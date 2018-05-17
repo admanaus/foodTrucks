30bb885f343fcea3e684c86dd2b1b268213fc0ba
Adrien Donat <I23332@xactware.com>
2018-05-17 10:10:17 -0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { UsersComponent } from './users/users.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent,
+    UsersComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule
